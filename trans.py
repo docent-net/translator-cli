3b869d21c0ae202761e98465ca68fb2936fd2366
Maciej Lasyk <maciek@lasyk.info>
2015-10-12 15:35:53 +0200

    init

---
+++ trans.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import simplejson as json
+import requests
+from sys import argv
+import sys
+
+if(len(argv) != 3):
+    print "Usage:\n\n python trans.py en-pl string"
+    sys.exit()
+
+_script, lngs, trans_string = argv
+
+
+APIKEY=open('APIKEY','r').read()
+
+lngs = lngs.split('-')
+
+URL = 'https://www.googleapis.com/language/translate/v2?key='+APIKEY+'&q='+trans_string +\
+      '&source='+lngs[0]+'&target='+lngs[1]
+
+data = json.loads(requests.get(URL).text)
+
+print data['data']['translations'][0]['translatedText']
